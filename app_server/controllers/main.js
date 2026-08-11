e13809f28a8cdfa60a3d5a56d4347caba10f58a6
mrlow4 <marlowvandyke@gmail.com>
2026-08-11 00:48:32 -0400

    error, forgot to checkout. style changes.

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -6,7 +6,7 @@ const index = (req, res) => {
         (donations.currentAmount / donations.goalAmount) * 100
     );
 
-    res.render("temp", {
+    res.render("index", {
         title:"Clubhouse Commons Main Page",
         currentAmount: donations.currentAmount,
         goalAmount: donations.goalAmount,
